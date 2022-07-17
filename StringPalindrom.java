b4f7c8487f6dcd334032b66a46f2b66f5ad575be
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-07-17 17:36:56 +0530

    String Palindrom

---
+++ StringPalindrom.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class StringPalindrom{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringPalindrom obj = new StringPalindrom();
+        System.out.println(obj.Palin(str));
+    }
+    
+     public boolean Palin(String s){
+         
+         String temp=s, emptyStr="";//temp=mansi
+         char result='\0';
+         for(int i=0; i<s.length(); i++)//i=0, i=1,
+         {
+             result= s.charAt(i);//result=m,result=a,
+             emptyStr=result+emptyStr;//emptyStr=am
+             
+         }
+         if(temp.equals (emptyStr))
+         return true;
+         else
+         return false;
+     }
+    
+}
