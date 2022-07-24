11eb45d11d0015bfd593b37f45c92ea4a75bdf1b
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-07-24 23:49:27 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Fibonacci{
+    
+    public static void Fibo(int num){
+        
+        // base condition 
+        if(num<=0)
+        {
+            System.out.println("enter valid no");
+        }
+        int f1 =0, f2=1, f3=0;
+         System.out.println(f1);//0,1
+         System.out.println(f2);
+       
+        for(int i=0; i<num-2; i++)//i<2
+        {
+             f3=f1+f2;//1, 
+             f1=f2;
+             f2=f3;
+              System.out.println(f3);//1,
+        }
+    }
+    
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Fibo(n);
+        
+    }
+}
