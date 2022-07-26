bb129e2fb4247a9e03556e7b4c51b401a97160fc
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-07-26 23:18:07 +0530

    Maximum Occ Latter

---
+++ MaximumOccLatter.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+
+public class MaximumOccLatter
+{
+    
+    
+     public static char MaxOccLatter(String sr)//test
+     
+     {
+         int [] arr = new int[256];
+         
+        //  if(sr.length()==0)
+        //  return '';
+         
+         for(int i=0; i<sr.length(); i++)
+         {
+             arr[sr.charAt(i)]++;
+             
+           }
+           int count=-1;
+           char ch='\0';
+            for(int j=0; j<sr.length(); j++)
+            {
+                if(count<arr[sr.charAt(j)])
+                {
+                    count=arr[sr.charAt(j)];
+                    ch = sr.charAt(j);
+                }
+                
+            }
+            return  ch;
+         
+         
+         
+         
+     }
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    String str = sc.next();
+	    
+		System.out.println(MaxOccLatter(str));
+	}
+}
