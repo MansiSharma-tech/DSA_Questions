3ee55096bfdfbb258c50aed9a81d1bd430c20c4f
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-08-08 06:37:12 +0530

    Common Element Between Two Array

---
+++ CommonElementOfTwoArray.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+ 
+class CommonElementOfTwoArray{
+ 
+    private static void findCommonElements(int[] arr1,
+                                           int[] arr2)
+    {
+         
+        // Check if length of arr1 is greater than 0
+        // and length of arr2 is greater than 0
+        if (arr1.length > 0 && arr2.length > 0) {
+            Set<Integer> firstSet = new HashSet<Integer>();
+            for (int i = 0; i < arr1.length; i++) {
+                firstSet.add(arr1[i]);
+            }
+           
+            // Iterate the elements of the arr2
+            for (int j = 0; j < arr2.length; j++) {
+                if (firstSet.contains(arr2[j])) {
+                    System.out.println(arr2[j]);
+                }
+            }
+        }
+    }
+   
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int[] arr1 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int[] arr2 = new int[] { 1, 3, 4, 5, 6, 9, 8 };
+       
+        // Function Call
+        findCommonElements(arr1, arr2);
+    }
+}
