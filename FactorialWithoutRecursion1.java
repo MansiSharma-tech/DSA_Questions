000d6d1326221c6adf33387155c660b22bb56f8d
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-08-28 23:15:45 +0530

    Factorial Without Recursion

---
+++ FactorialWithoutRecursion1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FactorialWithoutRecursion1{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        System.out.println(Fact(num));
+        
+    }
+    
+    public static int Fact(int num){
+        int result=1;
+        
+        for(int i=2; i<=num; i++)
+        {
+            result *= i;
+           
+        }
+        return result;
+        
+    }
+}
