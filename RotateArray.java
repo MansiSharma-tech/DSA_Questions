4e8303cb3288472e11444aaf6faaaf4bef48cabf
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-09-01 23:54:58 +0530

    for(int i=0;i<col;i++)

---
+++ RotateArray.java
@@ -0,0 +1,13 @@
+for(int i=0;i<col;i++)
+        {
+            for(int j=0; j<row; j++)
+            {
+                // int temp = arr[row][col];
+                // arr[row][col]=arr[col][row];
+                // arr[col][row]=temp;
+                   
+                System.out.print(arr[j][i]+" ");
+                
+            }
+            System.out.println(" ");
+	   }
