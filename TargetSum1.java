e3ed2176caab1a5ce8f2d6357c6763701eaa5699
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-09-13 23:31:32 +0530

    Add files via upload

---
+++ TargetSum1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class TargetSum1{
+    
+    public static void FindTarget(int [] arr, int targetValue)
+    
+    {
+        //other way to print "target sum not found" using "flag"
+        //int flag;
+        //base condition 
+        if(arr.length==0)
+        {
+            System.out.println("Enter valid array");
+        }
+        for(int i=0; i<arr.length-1;i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i]+arr[j]==targetValue)
+                {
+                    System.out.println("("+ i + " ," + j +")");
+                    return;
+                    //flag++;
+                }
+                
+
+            }
+            
+        }
+    //     
+    //   if(flag==0)
+    //   {
+    //         System.out.println("target sum not found");
+    //   }
+         System.out.println("target sum not found");
+        
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int size = sc.nextInt();
+        System.out.println("Enter the element of an array");
+        
+        int [] arr = new int [size];
+        for(int i=0; i<size; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        
+         System.out.println("Enter the target value");
+         int targetValue = sc.nextInt();
+         
+         FindTarget(arr, targetValue);
+    }
+}
