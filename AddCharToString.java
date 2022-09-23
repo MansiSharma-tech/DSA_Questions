adb6935e7e77128eb9fbe678b4326fd521aa2a3c
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-09-23 23:16:12 +0530

    Add files via upload

---
+++ AddCharToString.java
@@ -0,0 +1,18 @@
+
+
+public class AddCharToString
+{
+	public static void main(String[] args) {
+	
+        // Input character and string
+        char a = 's';
+        String str = "GeeksforGeek";
+ 
+        // Inserting at the end
+        String str2 = str + a;
+ 
+        // Print and display the above string
+        System.out.println(str2);
+    
+	}
+}
