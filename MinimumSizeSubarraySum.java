35af9b2da011e4a835df17de428297e2622c3e12
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-10-03 23:45:40 +0530

    Add files via upload

---
+++ MinimumSizeSubarraySum.java
@@ -0,0 +1,32 @@
+class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while(j < nums.length){
+            sum += nums[j];
+            if(sum < target)
+            {
+                j++;
+            }
+            else if(sum >= target){
+                while(i <= j){
+                    if(sum - nums[i] >= target){
+                        sum = sum - nums[i];
+                        i++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if(j - i + 1 < min){
+                    min = j - i + 1;
+                }
+                j++;
+            }
+        }
+        if(min == Integer.MAX_VALUE)return 0;
+        return min;
+    }
+}
