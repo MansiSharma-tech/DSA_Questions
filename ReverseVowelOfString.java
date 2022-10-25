e056ce034cecee4a8a3e563ec83d9cae20485ff1
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-10-26 00:02:46 +0530

    Update ReverseVowelOfString.java

--- ReverseVowelOfString.java
+++ ReverseVowelOfString.java
@@ -1,22 +1,22 @@
-class ReverseVowelOfString {
-   public String reverseVowels(String s) {       
+class Solution {
+   public String reverseVowels(String s) {//hello       
       	HashSet<Character> chars = new HashSet<Character>();
         chars.add('a'); chars.add('e'); chars.add('i'); chars.add('o'); chars.add('u');
         chars.add('A'); chars.add('E'); chars.add('I'); chars.add('O'); chars.add('U');
         int left = 0, right = s.length() - 1;  
         char[] sc = s.toCharArray();
         boolean leftVowel = false, rightVowel = false;
-        while (left < right) {
-            leftVowel = chars.contains(sc[left]);
-            rightVowel = chars.contains(sc[right]);
+        while (left < right) {//0<4,1<4,2<3,3<2
+            leftVowel = chars.contains(sc[left]);//lv=false,Lv=true,false
+            rightVowel = chars.contains(sc[right]);// true, true,false
             
             if (leftVowel && rightVowel) {
-                char swapper = sc[left];
-                sc[left++] = sc[right];
-                sc[right--] = swapper;
+                char swapper = sc[left];//
+                sc[left++] = sc[right];//0,2
+                sc[right--] = swapper;//3
             } else {
-                right -= (rightVowel) ? 0 : 1;
-                left += (leftVowel) ? 0 : 1;
+                right -= (rightVowel) ? 0 : 1;//4,2
+                left += (leftVowel) ? 0 : 1;//1,3, 
             }
         }
             
