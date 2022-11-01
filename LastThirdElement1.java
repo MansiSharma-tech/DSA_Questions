c68c872265c136a7f5338f7990e6ed46fe9c2f76
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-11-01 23:57:46 +0530

    Add files via upload

---
+++ LastThirdElement1.java
@@ -0,0 +1,10 @@
+class LastThirdElement1
+{
+public static void main(String args[])
+{
+int a[]=new int[5];
+int arr [] ={1,2,3,4,5};
+int size=a.length;
+System.out.println("Last element of an array is "+arr[size-3]);
+}
+}
