8500f6239d55d360b791ad74d117c51c12bbb33d
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2022-12-09 22:51:54 +0530

    Add files via upload

---
+++ CountWordInString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CountWordInString
+{
+	public static void main(String[] args) {
+		System.out.println("Enter String : ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		String arr[] = str.split(" ");
+		System.out.println(arr.length);
+		
+		
+		
+	}
+}
