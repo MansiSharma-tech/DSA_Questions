1beb474b82a433dd4935979aa99b7e20c8ef292e
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-01-28 19:56:45 +0530

    Add files via upload

---
+++ PowerofTwo.java
@@ -0,0 +1,13 @@
+public class PowerofTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0){
+            return false;
+        }
+        else if ((n & (n - 1)) == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }}
+  
