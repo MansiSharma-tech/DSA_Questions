2f692d5ae0bea5bae7f50ba1b40a2d0be8d6899e
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-04-20 22:35:24 +0530

    Add files via upload

---
+++ MaximumWidthOfBinaryTree.java
@@ -0,0 +1,28 @@
+class MaximumWidthOfBinaryTree {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<NodeColIndex> queue = new LinkedList<>();
+        queue.add(new NodeColIndex(root, 0));
+        int maxWidth = 0;
+        while (!queue.isEmpty()) {
+            NodeColIndex head = queue.peek();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                NodeColIndex removed = queue.remove();
+                TreeNode node = removed.node;
+                if (node.left != null) {
+                    queue.add(new NodeColIndex(node.left, 2 * removed.colIdx));
+                }
+                if (node.right != null) {
+                    queue.add(new NodeColIndex(node.right, 2 * removed.colIdx + 1));
+                }
+                maxWidth = Math.max(maxWidth, removed.colIdx - head.colIdx + 1);
+            }
+        }
+        return maxWidth;
+    }
+    
+    private record NodeColIndex(TreeNode node, int colIdx) {}
+}
