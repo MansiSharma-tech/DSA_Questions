5550ab6b71ace500de1ea496bd8d6d763a8c7e83
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-06-19 06:14:37 +0530

    Add files via upload

--- UglyNumber.java
+++ UglyNumber.java
@@ -1,22 +1,23 @@
-class UglyNumber {
-    public boolean isUgly(int n) {//14
-        if(n<=0)
-            return false;
-        
-        while(n%2==0)
-        {
-            n=n/2;//7
+class UglyNumber{
+    public int nthUglyNumber(int n, int a, int b, int c) {
+        int lo = 1, hi = (int)2e9;
+        long ab = lcm(a, b), ac = lcm(a, c), bc = lcm(b, c), abc = lcm(ab, c);
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+            long count = 0L + mid / a + mid / b + mid / c - mid / ab - mid / ac - mid / bc + mid / abc;
+            if (count >= n)
+                hi = mid;
+            else
+                lo = mid + 1;
         }
-        
-         while(n%3==0)
-        {
-            n=n/3;//1
-        }
-        
-         while(n%5==0)
-        {
-            n=n/5;
-        }
-        return n==1;
+        return lo;
+    }
+
+    private long gcd(long a, long b){
+        return b == 0? a : gcd(b, a % b);
+    }
+
+    private long lcm(long a, long b){
+        return a * b / gcd(a, b);
     }
 }
