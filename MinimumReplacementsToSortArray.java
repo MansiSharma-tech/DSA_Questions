a934c45ea3e48df59ffa8401a78d4c0cda745bea
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-08-31 00:17:18 +0530

    Add files via upload

---
+++ MinimumReplacementsToSortArray.java
@@ -0,0 +1,15 @@
+
+class MinimumReplacementsToSortArray {
+  public long minimumReplacement(int[] nums) {
+    long ans = 0;
+
+    int max = nums[nums.length - 1];
+    for (int i = nums.length - 2; i >= 0; --i) {
+      final int ops = (nums[i] - 1) / max;
+      ans += ops;
+      max = nums[i] / (ops + 1);
+    }
+
+    return ans;
+  }
+}
