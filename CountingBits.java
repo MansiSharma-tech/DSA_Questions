bd5bb06733720cc702d34811199badc65ce65095
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-09-01 15:36:23 +0530

    Add files via upload

---
+++ CountingBits.java
@@ -0,0 +1,13 @@
+class CountingBits
+ {
+  public int[] countBits(int n) {
+    // Let f(i) := i's # of 1's in bitmask
+    // f(i) = f(i / 2) + i % 2
+    int[] ans = new int[n + 1];
+
+    for (int i = 1; i <= n; ++i)
+      ans[i] = ans[i / 2] + (i % 2 == 0 ? 0 : 1);
+
+    return ans;
+  }
+}
