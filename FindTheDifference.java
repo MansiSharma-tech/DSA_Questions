a55987d2b68ddf4b9782ff25beddd66e0da8b3f4
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-09-25 21:41:18 +0530

    Add files via upload

---
+++ FindTheDifference.java
@@ -0,0 +1,13 @@
+class  FindTheDifference {
+  public char findTheDifference(String s, String t) {
+    char ans = 0;
+
+    for (final char c : s.toCharArray())
+      ans ^= c;
+
+    for (final char c : t.toCharArray())
+      ans ^= c;
+
+    return ans;
+  }
+}
