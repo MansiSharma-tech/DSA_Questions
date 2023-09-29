51d8cb60f70013b159c64101af102ddff45ed287
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-09-29 23:03:15 +0530

    Add files via upload

--- MonotonicArray.java
+++ MonotonicArray.java
@@ -1,35 +1,19 @@
-import java.util.*;
-public class MonotonicArray
-{
-     public boolean monotonus(int [] nums, int size)
-     {
-         boolean increasing = true;
-			boolean decreasing = true;
-       	//  [1,3,2]
-			for(int i=0;i<size-1;i++){
-				if(nums[i]<nums[i+1])//1<3, 3<2
-				{
-					decreasing =false;//f,f
-				}
-				if(nums[i]>nums[i+1])//1>3,3>2
-				{
-					increasing =false;//t,f
-				}
-			}
-    
-			return increasing || decreasing;
-     }
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	   	System.out.println("Enter size of an array ");
-	    int size = sc.nextInt();
-	    
-	     int [] arr = new int [size] ;
-	     for(int i=0; i<=size; i++)
-	    {
-	        arr[i] = sc.nextInt();
-	    }
-	    
-	
-	}
+class MonotonicArray {
+    public boolean isMonotonic(int[] nums) {
+        int idx = 0;
+        while (idx + 1 < nums.length && nums[idx] == nums[idx + 1]) {
+            idx++;
+        } 
+        boolean increasing = idx + 1 < nums.length && nums[idx] < nums[idx + 1];
+        while (idx + 1 < nums.length) {
+            if (nums[idx] > nums[idx + 1] && increasing) {
+                return false;
+            }
+            if (nums[idx] < nums[idx + 1] && !increasing) {
+                return false;
+            }
+            idx++;
+        }
+        return true;
+    }
 }
