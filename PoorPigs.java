978f986a4ae3b6fb7aaabcc9c5d578d1f2ca6171
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-10-29 22:25:23 +0530

    Add files via upload

---
+++ PoorPigs.java
@@ -0,0 +1,5 @@
+class  PoorPigs {
+  public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    return (int) Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie + 1));
+  }
+}
