25832d8f4ddd47d0fcf1cf9b472f0bd17132246a
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-11-03 12:42:35 +0530

    Add files via upload

---
+++ BuildAnArrayWithStackOperations.java
@@ -0,0 +1,15 @@
+class BuildAnArrayWithStackOperations
+ {
+    public List<String> buildArray(int[] target, int n) {
+        int cur = 0;
+        List<String> ans = new ArrayList<>();
+        for (int v : target) {
+            while (++cur < v) {
+                ans.add("Push");
+                ans.add("Pop");
+            }
+            ans.add("Push");
+        }
+        return ans;
+    }
+}
