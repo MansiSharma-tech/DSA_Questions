4e5d0510ed3e6284469c0d958e48bcaa232eb3e3
Mansi Sharma <64724394+MansiSharma-tech@users.noreply.github.com>
2023-12-10 23:36:11 +0530

    Add files via upload

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -1,46 +1,11 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
+class TransposeMatrix {
+  public int[][] transpose(int[][] A) {
+    int[][] ans = new int[A[0].length][A.length];
 
+    for (int i = 0; i < A.length; ++i)
+      for (int j = 0; j < A[0].length; ++j)
+        ans[j][i] = A[i][j];
 
-
-class TransposeMatrix
-{ 
-	static int n = 4;
-
-	static void swap(int mat[][], int i, int j)
-	{
-			int temp = mat[i][j];
-			mat[i][j] = mat[j][i];
-			mat[j][i] = temp;
-	}
-
-	static void transpose(int mat[][])
-	{
-
-		for(int i = 0; i < n; i++)
-			for(int j = i + 1; j < n; j++)
-				swap(mat, i, j);
-	}
-
-	public static void main(String args[]) 
-    {
-        int arr[][] = {{1, 2, 3, 4},
-    				   {5, 6, 7, 8},
-    				   {9, 10, 11, 12},
-    				   {13, 14, 15, 16}};
-
-    	transpose(arr);
-
-    		for(int i = 0; i < n; i++)
-			{
-				for(int j = 0; j < n; j++)
-				{
-					System.out.print(arr[i][j]+" ");
-				}
-
-				System.out.println();
-			}	
-    } 
-
+    return ans;
+  }
 }
